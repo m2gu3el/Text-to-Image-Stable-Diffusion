52f16ef27d8d0d7bf19c8d4fe2bfd5d11ef396f2
= <=>
2024-06-15 20:05:28 +0530

    Change

--- Decoder.py
+++ Decoder.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from Attentionttention import SelfAttention
+from Attention import SelfAttention
 
 class VAE_AttentionBlock(nn.Module):
     def __init__(self, channels):
