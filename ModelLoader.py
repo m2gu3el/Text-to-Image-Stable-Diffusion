d160691488660c86718a2b863cee3dcf93279e53
= <=>
2024-06-16 02:08:48 +0530

    ddpm model

---
+++ ModelLoader.py
@@ -0,0 +1,28 @@
+from Clip import CLIP
+from Encoder import VAE_Encoder
+from Decoder import VAE_Decoder
+from Diffusion import Diffusion
+
+import ModelConverter
+
+def preload_models_from_standard_weights(ckpt_path, device):
+    state_dict = ModelConverter.load_from_standard_weights(ckpt_path, device)
+
+    encoder = VAE_Encoder().to(device)
+    encoder.load_state_dict(state_dict['encoder'], strict=True)
+
+    decoder = VAE_Decoder().to(device)
+    decoder.load_state_dict(state_dict['decoder'], strict=True)
+
+    diffusion = Diffusion().to(device)
+    diffusion.load_state_dict(state_dict['diffusion'], strict=True)
+
+    clip = CLIP().to(device)
+    clip.load_state_dict(state_dict['clip'], strict=True)
+
+    return {
+        'clip': clip,
+        'encoder': encoder,
+        'decoder': decoder,
+        'diffusion': diffusion,
+    }
